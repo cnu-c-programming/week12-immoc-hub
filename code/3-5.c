682670a2f283a912d660d67ed7614e87a217c5de
ImmoC <ksy96782536@gmail.com>
2026-05-28 12:39:25 +0900

    3

--- code/3-5.c
+++ code/3-5.c
@@ -8,9 +8,18 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
     int sum = 0;
+    char line[128];
 
+    while (fgets(line,sizeof line, fp)!=0){
+        int num;
+        if (sscanf(line,"%d",&num)!=1){
+            fprintf(stderr,"invalid input %s", line);
+
+        }
+        else 
+            sum+=num;
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
